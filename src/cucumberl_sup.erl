7b55c5974feb58e9085be83b1aa21b0c5d9b0252
Oliver Ferrigni <oliver.ferrigni@gmail.com>
2011-04-12 10:48:00 -0500

    Added cucumber_server:start() to the init for cucumberl_sup.  This allows the cucumber_server to be started on application:start/1.
    Oliver

--- src/cucumberl_sup.erl
+++ src/cucumberl_sup.erl
@@ -24,5 +24,6 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
-    {ok, { {one_for_one, 5, 10}, []} }.
+	Children = [?CHILD(cucumber_server, worker)],
+    {ok, { {one_for_one, 5, 10}, Children} }.
 
