4e548d4ec9fdff570a98089780131ab7323e541a
Oliver Ferrigni <oliver.ferrigni@gmail.com>
2011-04-17 12:41:48 -0500

    First test to demonstrate parsing using mochiweb.
    Oliver

--- test/parsing_test.erl
+++ test/parsing_test.erl
@@ -8,3 +8,11 @@
 %[A,{_,[B]}] = mochijson2:decode("[\"snippet_text\",{\"step_keyword\":\"Given\",\"multiline_arg_class\":\"\",\"step_name\":\"we're all wired\"}]").
 
 
+parse_step_matches_test() ->
+	InputJson = "[\"step_matches\",{\"name_to_match\":\"we're all
+	wired\"}]",
+	StepMatches = <<"step_matches">>,
+	NameToMatch = <<"name_to_match">>,
+	[StepMatches,{struct, [{NameToMatch, Name}]}] = mochijson2:decode(InputJson),
+	?assertEqual(<<"we're all\n\twired">>, Name).
+
