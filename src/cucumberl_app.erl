41d989bf39bbdb634a3c17734fe270dae4208537
Oliver Ferrigni <oliver.ferrigni@gmail.com>
2011-06-07 21:50:31 -0500

    Correcteed loading of app.
    Oliver

--- src/cucumberl_app.erl
+++ src/cucumberl_app.erl
@@ -4,13 +4,18 @@
 
 %% Application callbacks
 -export([start/2, stop/1]).
+-export([start/0]).
 
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
-    cucumberl_sup:start_link().
+  cucumberl_sup:start_link().
+
+start() ->
+  application:load(cucumberl),
+  application:start(cucumberl).
 
 stop(_State) ->
     ok.
