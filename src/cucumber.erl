5dc1c4807e8bcffbb17c1c0f985d6f04a4b964dd
Oliver Ferrigni <oliver.ferrigni@gmail.com>
2011-06-13 12:26:04 -0500

    Missed referencing when moving function.
    Oliver

--- src/cucumber.erl
+++ src/cucumber.erl
@@ -23,13 +23,13 @@ execute_json(Json, AllStepModules) ->
 		{ok, end_scenario, _Tags} -> {ok, noreply};
     {ok, snippet_text, _Keyword, Name} -> 
       ListName = binary:bin_to_list(Name),
-      CucumberAtoms = cucumberl:string_to_atoms(ListName),
+      CucumberAtoms = utils:string_to_atoms(ListName),
       %CompleteList = "For " ++ ListKeyword ++ " Please implement step(" ++ ListName  ++  ")\n and step("++ ListName++ ", matches)",
       JoinedAtoms = string:join(lists:map(fun(Elem) -> atom_to_list(Elem) end, CucumberAtoms), ","),
       CompleteList = "step(" ++ JoinedAtoms  ++  ")\nstep("++ JoinedAtoms ++ ", matches)->ok;",
       End = mochijson2:encode([success, list_to_atom(CompleteList)]),
       {ok, End};
-		{ok, invoke, Name, _Args} ->	StepAtoms = cucumberl:string_to_atoms(binary:bin_to_list(Name)),
+		{ok, invoke, Name, _Args} ->	StepAtoms = utils:string_to_atoms(binary:bin_to_list(Name)),
       discovery:run_steps(AllStepModules, StepAtoms);
     {ok, new_line} -> {ok, new_line};
 		_ -> undefined
