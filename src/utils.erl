e94868795061c878b705f19fccd83c89a4556ab7
Oliver Ferrigni <oliver.ferrigni@gmail.com>
2011-06-13 08:56:21 -0500

    Pulled function into utils module.
    Oliver

--- src/utils.erl
+++ src/utils.erl
@@ -4,4 +4,9 @@
 
 
 bitstring_to_atoms(FromList) ->
-  cucumberl:string_to_atoms(binary:bin_to_list(FromList)).
+  string_to_atoms(binary:bin_to_list(FromList)).
+
+
+string_to_atoms(StrWords) ->
+    lists:map(fun (Y) -> list_to_atom(string:to_lower(Y)) end,
+              string:tokens(StrWords, " ")).
